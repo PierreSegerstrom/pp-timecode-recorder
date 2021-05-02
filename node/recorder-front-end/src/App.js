124354e67d4ec7b002e3cd844d480741fdc87775
Pierre Segerström <pise@kth.se>
2021-05-02 21:34:40 +0200

    Animation of 'file form'

--- node/recorder-front-end/src/App.js
+++ node/recorder-front-end/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useTransition, animated } from 'react-spring'
+
 import ActionButton from './components/ActionButton';
 import FileHandlingForm from './components/FileHandlingForm';
 import './App.css';
@@ -7,15 +9,27 @@ import './App.css';
 const App = () => {
     const [showFileForm, setShowFileForm] = useState(false);
 
+    const slideUpFade = useTransition(showFileForm, {
+        from:   { y: 100,   opacity: 0 },
+        enter:  { y: 0,     opacity: 1 },
+        leave:  { y: 100,   opacity: 0 }
+    });
+
     return (
         <div className="App">
             <ActionButton
                 revealFileForm = { () => setShowFileForm(true) }
             />
-            {showFileForm &&
-            <FileHandlingForm
-                hideFileForm = { () => setShowFileForm(false) }
-            />}
+            {slideUpFade(({y, opacity}, item) => (
+                item &&
+                <animated.div style={{ opacity: opacity }} className="overlay">
+                    <animated.div style={{ y: y }}>
+                        <FileHandlingForm
+                            hideFileForm = { () => setShowFileForm(false) }
+                        />
+                    </animated.div>
+                </animated.div>
+            ))}
         </div>
     );
 }
